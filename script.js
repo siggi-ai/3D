e88ca1edb3f5e0ef0e8639b2fd451ff4a2952d5a
siggi <siegfried.hamm@gmail.com>
2022-06-15 16:34:04 +0200

    red cube

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ const sizes = {
 
 //Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+/* camera.position.x = 1
+camera.position.y = -1 */
 scene.add(camera)
 
 //Renderer

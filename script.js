a0b724aef4f682b6d915a3056b630cad352b7552
siggi <siegfried.hamm@gmail.com>
2022-06-16 10:02:21 +0200

    .

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ scene.background = new THREE.Color(0x00ff00)
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+mesh.position.set(0, 0, 0)
 scene.add(mesh)
 
 //Sizes

c49613780c1281f042828b8ed3d7decb48b2f020
siggi <siegfried.hamm@gmail.com>
2022-06-17 08:40:59 +0200

    directional light

--- 3d1/script.js
+++ 3d1/script.js
@@ -4,7 +4,7 @@ scene.background = new THREE.Color(0x00ff00)
 
 //Red Cube
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshLambertMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 mesh.position.set(0, 0, 0)
 scene.add(mesh)
@@ -21,6 +21,13 @@ camera.position.z = 3
 camera.position.x = 1
 camera.position.y = -1
 scene.add(camera)
+
+var spotLight = new THREE.SpotLight(0xFFFFFF);
+spotLight.position.set(-40, 40, -15);
+spotLight.castShadow = true;
+spotLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+spotLight.shadow.camera.far = 130;
+spotLight.shadow.camera.near = 40;
 
 //Renderer
 const canvas = document.querySelector('.webgl')

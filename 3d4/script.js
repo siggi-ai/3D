69ca38721a63e07c5e7a5b08ad28fdca85cdad71
siggi <siegfried.hamm@gmail.com>
2022-06-19 11:35:51 +0200

    .

--- 3d4/script.js
+++ 3d4/script.js
@@ -4,37 +4,39 @@
     var renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(new THREE.Color(0x000000));
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.Enabled = true; 
+
 
     var axes = new THREE.AxesHelper(20);
     scene.add(axes);
 
     var planeGeometry = new THREE.PlaneGeometry(60, 20);
     var planeMaterial = new THREE.MeshLambertMaterial({
-        color: 0xAAAAAA
+        color: 0xAAAAAA,
     });
 
     var plane = new THREE.Mesh(planeGeometry, planeMaterial);
     plane.rotation.x = -0.5 * Math.PI;
-    plane.position.set(15, 0, 0);
+    plane.position.set(15, 0, 0); 
+    plane.receiveShadow = true; 
     scene.add(plane);
 
     var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
     var cubeMaterial = new THREE.MeshLambertMaterial({
         color: 0xFF0000,
-        /* wireframe: true */
     });
     var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
     cube.position.set(-4, 3, 0);
+    cube.castShadow = true;
     scene.add(cube);
 
-    // create a sphere
     var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
     var sphereMaterial = new THREE.MeshLambertMaterial({
         color: 0x7777FF,
-        /* wireframe: true */
     });
     var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
     sphere.position.set(20, 4, 2);
+    sphere.castShadow = true; 
     scene.add(sphere);
 
     var spotLight = new THREE.SpotLight(0xFFFFFF);
@@ -45,13 +47,15 @@
     spotLight.shadow.camera.near = 40;
     scene.add(spotLight);
 
-    // position and point the camera to the center of the scene
     camera.position.set(-30, 40, 30);
     camera.lookAt(scene.position);
 
-    // add the output of the renderer to the html element
     document.getElementById("webgl-output").appendChild(renderer.domElement);
 
-    // render the scene
     renderer.render(scene, camera);
+
+
+
+
+    
 

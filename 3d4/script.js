cfa6cb28cbccd29ac5c7c8149aaa41590ef406f0
siggi <siegfried.hamm@gmail.com>
2022-06-20 11:32:00 +0200

    .

--- 3d4/script.js
+++ 3d4/script.js
@@ -52,14 +52,29 @@
     camera.position.set(-30, 40, 30);
     camera.lookAt(scene.position);
 
+    //GUI
+    var controls = new function() { 
+        this.rotationSpeed = 0.02; 
+        this.bouncingSpeed = 0.03; 
+      } 
+
+    var gui = new dat.GUI(); 
+    gui.add(controls, 'rotationSpeed', 0, 0.5); 
+    gui.add(controls, 'bouncingSpeed', 0, 0.5); 
+
+    //Animation
     var step = 0;
 
     function renderScene() {
         stats.update();
-        cube.rotation.x += 0.02;
+        /* cube.rotation.x += 0.02;
         cube.rotation.y += 0.02;
         cube.rotation.z += 0.02;
-        step+=0.04; 
+        step+=0.04; */ 
+        cube.rotation.x += controls.rotationSpeed; 
+        cube.rotation.y += controls.rotationSpeed; 
+        cube.rotation.z += controls.rotationSpeed; 
+        step += controls.bouncingSpeed; 
         sphere.position.x = 20 + 10*(Math.cos(step)); 
         sphere.position.y = 2 + 10*Math.abs(Math.sin(step)); 
         requestAnimationFrame(renderScene);

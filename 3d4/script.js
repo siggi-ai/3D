46489cfe405f98ecf6cdedc169f4ad91aeab4afb
siggi <siegfried.hamm@gmail.com>
2022-06-20 10:54:00 +0200

    .

--- 3d4/script.js
+++ 3d4/script.js
@@ -52,11 +52,16 @@
     camera.position.set(-30, 40, 30);
     camera.lookAt(scene.position);
 
+    var step = 0;
+
     function renderScene() {
         stats.update();
         cube.rotation.x += 0.02;
         cube.rotation.y += 0.02;
         cube.rotation.z += 0.02;
+        step+=0.04; 
+        sphere.position.x = 20 + 10*(Math.cos(step)); 
+        sphere.position.y = 2 + 10*Math.abs(Math.sin(step)); 
         requestAnimationFrame(renderScene);
         renderer.render(scene, camera);
     }

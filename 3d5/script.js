df1d79bea802173d595550cd8d16a77abc3658fc
siggi <siegfried.hamm@gmail.com>
2022-06-22 13:36:56 +0200

    .

--- 3d5/script.js
+++ 3d5/script.js
@@ -6,10 +6,18 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
-const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+const material = new THREE.MeshLambertMaterial( { color: 0xffff00 } );
 const torusKnot = new THREE.Mesh( geometry, material );
 scene.add( torusKnot );
 
 camera.position.z = 40;
+
+var spotLight = new THREE.SpotLight(0xFFFFFF);
+    spotLight.position.set(-40, 40, -15);
+    spotLight.castShadow = true;
+    spotLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+    spotLight.shadow.camera.far = 130;
+    spotLight.shadow.camera.near = 40;
+scene.add(spotLight);
 
 renderer.render( scene, camera );
